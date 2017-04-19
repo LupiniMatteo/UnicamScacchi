a7cca3c4b2f120bb768a9aab8d45d4094db1025e
LupiniMatteo <matteo.lupini@studenti.unicam.it>
2017-04-19 10:59:48 +0200

    Aggiunti commenti

--- Modello/IScacchiera.cs
+++ Modello/IScacchiera.cs
@@ -4,7 +4,9 @@ namespace Scacchi.Modello
 {
     public interface IScacchiera
     {
+        //accesso sequenziale, che si scorre con un foreach che scorre tutte le case
         IEnumerable<ICasa> Case { get; }
+        //acesso casuale
         ICasa this[Colonna colonna, Traversa traversa] { get; }
     }
 }
--- Modello/Scacchiera.cs
+++ Modello/Scacchiera.cs
@@ -7,7 +7,7 @@ namespace Scacchi.Modello {
     {
         private ICasa[] listaCase;
 
-        /*public Scacchiera(){
+       /* public Scacchiera() {
             listaCase= new ICasa[64];
             int contatore=0;
             for (int i = 1; i < 9; i++)
@@ -24,8 +24,11 @@ namespace Scacchi.Modello {
         public Scacchiera()
         {
             listaCase = Enumerable
+            //butta fuori un elenco di numeri fino a 63
             .Range(0, 64)
+            //della fila di interi la posso trasformare usando la select, decidiamo noi cosa buttare fuori con la select
             .Select(i => (ICasa) new Casa((Colonna) (i%8+1), (Traversa) (i/8+1)))
+            //to array trasforma tutto in array
             .ToArray();
         }
         public IEnumerable<ICasa> Case {
